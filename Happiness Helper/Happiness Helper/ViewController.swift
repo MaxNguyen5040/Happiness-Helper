fad568b8bd99038694756642640c4d87e7def517
MaxNguyen5040 <63136073+MaxNguyen5040@users.noreply.github.com>
2021-08-02 14:51:22 -0500

    Add files via upload

---
+++ Happiness Helper/Happiness Helper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Happiness Helper
+//
+//  Created by Max Nguyen on 8/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
